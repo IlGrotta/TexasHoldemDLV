e646caccb7c4f608ce12a7a7c47abd718374a802
IlGrotta <45855047+ilgrotta@users.noreply.github.com>
2020-05-30 11:42:39 +0200

    First commit

---
+++ src/main/java/Test.java
@@ -0,0 +1,23 @@
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.setProperty("webdriver.gecko.driver","src/main/resources/geckodriver.exe");
+        FirefoxDriver f=new FirefoxDriver();
+
+        f.get("https://www.google.com");
+        f.manage().window().maximize();
+        String appTitle=f.getTitle();
+        String expTitle="Google";
+        if (appTitle.equals (expTitle)){
+            System.out.println("Verification Successfull");
+        }
+        else{
+            System.out.println("Verification Failed");
+
+        }
+        f.close();
+        System.exit(0);
+    }
+}
