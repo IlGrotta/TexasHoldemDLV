a8f4f3d077e5fcce0f9c195c17d3bf71384bc1f7
FazioFrancesco <55951469+faziofrancesco@users.noreply.github.com>
2020-06-03 16:57:37 +0200

    ciao

--- src/main/java/game/Main.java
+++ src/main/java/game/Main.java
@@ -11,89 +11,26 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.UnsupportedEncodingException;
 import java.sql.Driver;
 import java.util.regex.Pattern;
 
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
-        System.setProperty("webdriver.gecko.driver","src/main/resources/geckodriver.exe");
-        FirefoxOptions options=new FirefoxOptions();
-        options.addArguments("--disable-gpu\", \"--window-size=1920,1200\",\"--ignore-certificate-errors");
-        WebDriver driver=new FirefoxDriver(options);
-        driver.navigate().to("http://react-poker.surge.sh/");
-
-
-        //driver.findElement(By.className("fold-button")).click();
-
-        boolean playerexis;
-        boolean myTurn;
-        boolean win=Boolean.FALSE;
-        do{
-            playerexis=Boolean.FALSE;
-            do{
-
-                 myTurn=(driver.findElements( By.xpath("//button[@class='action-button']") ).size()!=0);
-            }while (!myTurn);
-
+    public static void main(String[] args) throws InterruptedException, UnsupportedEncodingException {
+        WebConnector prova=new WebConnector();
+        System.out.println(prova.getFirstCard().getKey());
+        System.out.println(prova.getFirstCard().getValue());
+        System.out.println(prova.getSecondCard().getKey());
+        System.out.println(prova.getSecondCard().getValue());
+        prova.closeWeb();
+    }
+     /*
             WebElement slider= (driver.findElement(By.xpath("//div[@class='slider-handles']/div")));
             Actions move=new Actions(driver);
             Action action=(Action) move.dragAndDropBy(slider,300    ,0).build();
             action.perform();
             Thread.sleep(2000);
             driver.findElement(By.xpath("//button[@class='action-button']")).click();
-            Thread.sleep(200);
-            //*[@id="root"]/div/div/div/div[1]/div[4]/div[3]/div/img
-            //*[@id="root"]/div/div/div/div[1]/div[2]/div[3]/div/img
-            //*[@id="root"]/div/div/div/div[1]/div[3]/div[3]/div/img
-
-            win=(driver.findElements( By.xpath("//*[@id=\"root\"]/div/div/div") ).size()!=0);
-            System.out.println(playerexis);
-            System.out.println(win);
-        }while(playerexis || !win);
-        System.out.println("s");
-       // driver.findElement(By.xpath("//div[@class='action-buttons']/button[1]")).click();
-        Thread.sleep(3000);
-        //String pot= driver.findElement(By.xpath("//div[@class='pot-container']/h4")).getText();
-      //  System.out.println(pot);
-
-
-
-
-        driver.close();
-
-
-        System.exit(0);
-
-
-
-
-
-    }
-   /* protected String checkSceltaAvversario()
-    {
-        Integer dump = getPrezzoCall();
-        if(dump == null)
-            return "";
-        //*[@id="root"]/div/div/div/div[1]/div[1]/div[1]
-        //*[@id="root"]/div/div/div/div[1]/div[3]/div[1]
-        //*[@id="root"]/div/div/div/div[1]/div[5]/div[3]/div/h5
-    }
-    protected Integer getPrezzoCall()
-    {
-        Integer value = null;
-        while(value == null)
-        {
-            try
-            {
-                value = Integer.valueOf(driver.findElement(By.xpath("/html[1]/body[1]/div[7]/font[1]/font[1]")).getText());
-            }catch(Exception e){
-                if(driver.findElement(By.id("call-button")).getText().equals("Go on"))
-                    return null;
-            }
-        }
-
-        return value;
-    }*/
-
+            Thread.sleep(200);*/
 }
--- src/main/java/game/WebConnector.java
+++ src/main/java/game/WebConnector.java
@@ -2,69 +2,67 @@ package game;
 import javafx.util.Pair;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.interactions.Action;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.sql.Driver;
-import java.util.HashMap;
-import java.util.regex.Pattern;
 public class WebConnector {
     WebDriver driver;
-    public WebConnector()
-    {
+    public WebConnector() throws InterruptedException {
 
         System.setProperty("webdriver.gecko.driver","src/main/resources/geckodriver.exe");
         FirefoxOptions options=new FirefoxOptions();
         options.addArguments("--disable-gpu\", \"--window-size=1920,1200\",\"--ignore-certificate-errors");
         driver=new FirefoxDriver(options);
         driver.navigate().to("http://react-poker.surge.sh/");
+        Thread.sleep(10000);
     }
     public int getPot()
     {
+
         Integer pot=Integer.parseInt((driver.findElement(By.xpath("//div[@class='pot-container']/h4")).getText()));
         return pot;
     }
-    public Pair<String,String> getFirstCard()
+    public Pair<String, String> getFirstCard()
     {
         Pair<String,String>card = null;
         String card1=(driver.findElement(By.xpath("//div[@class='player-entity--wrapper p0']/div[2]/div[1]/h6")).getText());
         String Number=card1.substring(0,card1.length()-2);
-        if(card1.charAt(card1.length())=='♣'){
+        if(card1.charAt(card1.length()-1)=='♣'){
             card=new Pair<String, String>("clubs",Number);
         }
-        if(card1.charAt(card1.length())=='♠'){
+        if(card1.charAt(card1.length()-1)=='♠'){
             card=new Pair<String, String>("diamonds",Number);
         }
-        if(card1.charAt(card1.length())=='♦'){
+        if(card1.charAt(card1.length()-1)=='♦'){
             card=new Pair<String, String>("spades",Number);
         }
-        if(card1.charAt(card1.length())=='♥'){
+        if(card1.charAt(card1.length()-1)=='♥'){
             card=new Pair<String, String>("hearts",Number);
         }
         return card;
     }
-    public Pair<String,String> getSecondCard()
+
+    public Pair<String, String> getSecondCard()
     {
         Pair<String,String>card = null;
-        String card1=(driver.findElement(By.xpath("//div[@class='player-entity--wrapper p0']/div[2]/div[1]/h6")).getText());
+        String card1=(driver.findElement(By.xpath("//div[@class='player-entity--wrapper p0']/div[2]/div[2]/h6")).getText());
         String Number=card1.substring(0,card1.length()-2);
-        if(card1.charAt(card1.length())=='♣'){
+        if(card1.charAt(card1.length()-1)=='♣'){
             card=new Pair<String, String>("clubs",Number);
         }
-        if(card1.charAt(card1.length())=='♠'){
+        if(card1.charAt(card1.length()-1)=='♠'){
             card=new Pair<String, String>("diamonds",Number);
         }
-        if(card1.charAt(card1.length())=='♦'){
+        if(card1.charAt(card1.length()-1)=='♦'){
             card=new Pair<String, String>("spades",Number);
         }
-        if(card1.charAt(card1.length())=='♥'){
+        if(card1.charAt(card1.length()-1)=='♥'){
             card=new Pair<String, String>("hearts",Number);
         }
         return card;
+    }
+    public void closeWeb()
+    {
+        driver.close();
     }
 }
